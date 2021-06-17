f34f0e1aa70982b4d0be6905d431202ea4539960
ojoago <dhasmom01@gmail.com>
2021-06-17 23:23:14 +0000

    try again

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,6 @@
  */
 int main()
 {
-/**
- * printf("n: %d: n%s");
-fflush(stdout); /* force it to go out */
 write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",61);
 return (1);
 }
