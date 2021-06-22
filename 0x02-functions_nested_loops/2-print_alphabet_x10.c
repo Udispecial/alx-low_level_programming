f7c2bda31e9ef33e2be8df079bcddff4e34bbf75
ojoago <dhasmom01@gmail.com>
2021-06-22 11:05:45 +0000

    debugging

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,24 +4,19 @@
  *
  * Return: x10 a-z
  */
+
 void print_alphabet_x10(void)
 {
 
 int n, co = 0;
 
 while (co < 10)
-
 {	
-
  for (n = 'a'; n <= 'z'; n++)
-
 {
 _putchar(n);
 }
-
 co++;
-
 _putchar('\n');
-
 }
 }
