406be8b3ff48b4aa0988459c6e1e167a8399f503
ojoago <dhasmom01@gmail.com>
2021-06-29 20:43:10 +0000

    commenting

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,17 @@
 #include "holberton.h"
 /**
- * main - swap int
+ * swap_int - Entry point
+ *@a: point the integer we want to swap
+ *@b: point the integer we want to swap
  *
  * Return: Always 0
  */
-void swap_int(int *a,int *b)
+void swap_int(int *a , int *b)
 {
 int tmp = *a;
+
 *a = *b;
+
 *b = tmp;
 
 }
