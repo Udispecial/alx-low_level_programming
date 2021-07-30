c8774702a53bce6d1a7b55fdda4861e8d869e923
ojoago <dhasmom01@gmail.com>
2021-07-30 17:12:37 +0000

    debugging

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 
 /**
- * list_len - entry point
- *	@h: get list
- * Return: quantity nodes
+ * list_len - finds the number of elements in a linked list
+ * @h: pointer to the start of the linked list
+ *
+ * Return: number of elements in the linked list
  */
 size_t list_len(const list_t *h)
 {
-size_t n_nodes = 0;
-while (h != NULL)
-{
-h = h->next;
-n_nodes++;
-}
-return (n_nodes);
+const list_t *tmp;
+unsigned int i;
+tmp = h;
+for (i = 0; tmp; i++)
+tmp = tmp->next;
+return (i);
 }
